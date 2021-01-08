af2ffe08f7592907571f388063bfee46d112d8cb
Yahya <yahya@skydu.id>
2021-01-09 03:38:07 +0700

    hide user id on comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,4 +10,22 @@ class Comment extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $hidden = ['user_id','user'];
+    protected $with = ['user'];
+    protected $appends = ['username', 'user_image_url'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getUsernameAttribute()
+    {
+        return $this->user->username;
+    }
+
+    public function getUserImageUrlAttribute()
+    {
+        return $this->user->image_url;
+    }
 }
